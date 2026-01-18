2ce328e72004eea4948c168857988db427ad3fec
tayyaba2555 <khantayyaba1004@gmail.com>
2026-01-18 21:42:29 +0530

    firstprogram,py

---
+++ firstprogram.py
@@ -0,0 +1,56 @@
+#--------input to integer------------
+
+um1= int(input("Enter first num1:",))
+num2= int(input("Enter second num2:",))
+print("sum:" ,num1 + num2)
+
+#------input to str---------
+
+str1 = "khan"
+str2 = "tayyaba"
+str = str1+" " +str2
+print(str)
+
+#--------input to float--------
+num1 = float(input("Enter the first val:" ,))
+num2 = float(input("Enter the second val:" ,))
+print("minus:" , num1 - num2)
+
+#-----------
+
+name = input("enter name:" ,)
+age = input("Enter age:" ,)
+marks = input("Enter marks of student:" ,)
+
+print("welcome" , name)
+print("age:" , age)
+print("Marks:" , marks)
+
+#write a program to input 2 numbers and print their sum
+
+a = int(input("Enter first number:" ,))
+b = int(input("Enter second number:" ,))
+sum = a + b
+print("sum:" , sum)
+
+#write a program to input side of a square and print its area 
+
+side = int(input("Enter side of square:" ,))
+rint("area:" , side * side)
+
+# wap to input 2 floating number and print their average 
+
+num1 = int(input("Enter num1:",))
+num2 = int(input("Enter num2:" ,))
+print("avg:" , num1 + num2 /2)
+
+#wap to input 2 int numbers  a and b print true . if a is greater than or equal to b . if not print false
+
+num1 = int(input("Enter first num:" ,))
+num2 = int(input("Enter second number:" ,))
+print(num1 >= num2)
+
+#-------chapter 2 : string ---------------
+
+str = "alfiya is the best woman in the wrold.\t she is beautiful in this world".
+print(str)
